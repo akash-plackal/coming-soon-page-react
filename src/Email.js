2802c1b5135d459826f7436bd1bb55d14df521ff
akash plackal <akashplackal@gmail.com>
2021-07-15 20:52:42 +0530

    almost done 2

--- src/Email.js
+++ src/Email.js
@@ -4,19 +4,17 @@ import "./email.css";
 export default function Email() {
   return (
     <div className="email">
-      <form>
-        <input type="email" name="email" placeholder="Email Address" />
-        <button type="submit">
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
-            <path
-              fill="none"
-              stroke="#FFF"
-              // stroke-width="2"
-              d="M1 1l8.836 8.836L1 18.671"
-            />
-          </svg>
-        </button>
-      </form>
+      <input type="email" placeholder="Email Address" />
+      <button type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20">
+          <path
+            fill="none"
+            stroke="#FFF"
+            // stroke-width="2"
+            d="M1 1l8.836 8.836L1 18.671"
+          />
+        </svg>
+      </button>
     </div>
   );
 }
